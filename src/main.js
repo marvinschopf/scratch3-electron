485e8b179a941ae0784045908016a056b0f9ad6c
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2018-06-18 22:12:23 +0200

    Update window size

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 1140, height: 815})
 
   // and load the index.html of the app.
   mainWindow.loadFile('src/scratch3-gui/index.html')
